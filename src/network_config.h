7d5586048b24691ec54630cd7820dfd7aac3b6b8
mirwin5 <mirwin5@gmail.com>
2019-08-25 12:04:47 -0500

    added network config modified sensor readings cpp and h

---
+++ src/network_config.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+#define IO_USERNAME "mirwin"
+#define IO_KEY "6329bdff250e4fe7b4639eea2edfb7dc"
+
+#define WIFI_SSID "APUS"
+#define WIFI_PASS "irwin#network"
+
+#include "AdafruitIO_WiFi.h"
--- src/sensor_readings.h
+++ src/sensor_readings.h
@@ -1,13 +1,19 @@
-
 #ifndef SENSOR_READINGS_H
 #define SENSOR_READINGS_H
 
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <TFT_eSPI.h>
+#include "settings.h"
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA (1015.15)
 
-void refresh_readings(Adafruit_BME280 bme);
+void refresh_readings(Adafruit_BME280 *bme,
+                      TFT_eSPI *tft,
+                      AdafruitIO_Feed *temp,
+                      AdafruitIO_Feed *hum,
+                      AdafruitIO_Feed *bar,
+                      AdafruitIO_Feed *alt);
 
 #endif
