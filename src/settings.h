2fb9960abce2d4fe3357760d9aae1e26223ffb6b
mirwin5 <mirwin5@gmail.com>
2019-09-11 11:01:14 -0500

    added IO Pins to settings.h

--- src/settings.h
+++ src/settings.h
@@ -22,6 +22,12 @@
 #include "mqtt_controller.h"
 
 #define LED_PIN 17
+#define S_OPEN_PIN 35
+#define S_CLOSE_PIN 34
+#define W_OPEN_PIN 33
+#define W_OPEN_PIN 32
+#define H_LEVEL_PIN 27
+#define A_LEVEL_PIN 25
 #define IFTTT_PIN 16
 #define EEPROM_SIZE 2                           // We'll use 1 byte in the EEPROM to store the max number of POSTs
 #define CALIBRATION_FILE "/tft-touch-calData_1" // This file will contain the data from the
