e1103d097434c51a4166180c97c7d0ab51602e77
mirwin <mirwin5@gmail.com>
2019-10-25 12:49:15 -0500

    changed screen layout and added level sensor reading and system condition

--- src/sensor_readings.cpp
+++ src/sensor_readings.cpp
@@ -25,45 +25,61 @@ void refresh_readings(Adafruit_BME280 *bme,
     f_altitude = bme->readAltitude(SEALEVELPRESSURE_HPA) * 3.28084;
 
     tft->setTextColor(TFT_YELLOW, bg);
-    tft->loadFont("SansSerif-36");
+    tft->loadFont("SansSerif-24");
 
     // Temperature
     Serial.print(f_temperature);
     Serial.println(" °F");
-    tft->fillRect(5, 50, 140, 30, bg);
-    tft->setCursor(5, 50);
+    tft->fillRect(5, 30, 140, 30, bg);
+    tft->setCursor(5, 30);
     tft->print(f_temperature);
     tft->println(" °F");
 
     // Humidity
     Serial.print(f_humidity);
     Serial.println(" %");
-    tft->fillRect(5, 90, 130, 30, bg);
-    tft->setCursor(5, 90);
+    tft->fillRect(5, 60, 130, 30, bg);
+    tft->setCursor(5, 60);
     tft->print(f_humidity);
     tft->println(" %");
 
     // Pressure
     Serial.print(f_pressure);
     Serial.println(" inhg");
-    tft->fillRect(5, 130, 200, 30, bg);
-    tft->setCursor(5, 130);
+    tft->fillRect(5, 90, 200, 30, bg);
+    tft->setCursor(5, 90);
     tft->print(f_pressure);
     tft->println(" inhg");
 
+ // Level Sensor
+    Serial.print("500");
+    Serial.println(" ma");
+    tft->fillRect(5, 120, 200, 30, bg);
+    tft->setCursor(5, 120);
+    tft->print("500");
+    tft->println(" ma");
+
+     // Condition
+    Serial.print("Open");
+    Serial.println(" Condition");
+    tft->fillRect(5, 150, 200, 30, bg);
+    tft->setCursor(5, 150);
+    tft->print("Open");
+    tft->println(" Condition");
+
     // Appx altitude
-    Serial.print(f_altitude);
-    Serial.println(" ft");
-    tft->fillRect(5, 170, 200, 30, bg);
-    tft->setCursor(5, 170);
-    tft->print(f_altitude);
-    tft->println(" ft");
+    // Serial.print(f_altitude);
+    // Serial.println(" ft");
+    // tft->fillRect(5, 170, 200, 30, bg);
+    // tft->setCursor(5, 170);
+    // tft->print(f_altitude);
+    // tft->println(" ft");
 
     //Send data to Adafruit.IO
     temp->save(f_temperature);
     hum->save(f_humidity);
     bar->save(f_pressure);
-    alt->save(f_altitude);
+    // alt->save(f_altitude);
 
     // Update the postsCounter value in the EEPROM and the TFT
     postsCounter(tft);
