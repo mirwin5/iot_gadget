b8f464f7a55a228d88da4ce6e7d2d57ac2958814
mirwin5 <mirwin5@gmail.com>
2020-09-21 10:26:13 -0500

    changed network settings

--- src/network_config.h
+++ src/network_config.h
@@ -3,7 +3,7 @@
 #define IO_USERNAME "mirwin"
 #define IO_KEY "6329bdff250e4fe7b4639eea2edfb7dc"
 
-#define WIFI_SSID "G100MLI"
-#define WIFI_PASS "irwin#network"
+#define WIFI_SSID "TresChickens"
+#define WIFI_PASS "TinyFarm2"
 
 #include "AdafruitIO_WiFi.h"
